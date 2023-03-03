56ce22eaa206aa9cb49f06c0eb1be0143c2935e9
Максим Полушин <shacolol@mail.ru>
2023-03-03 14:52:26 +0300

    этот коммит добовляет слайдер

---
+++ 14_javascript/js/hero-slider.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.js-hero-slider', {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: '.js-hero-pagination',
+    clickable: true,
+  },
+
+  a11y: {
+    paginationBulletMessage: 'Перейти к слайду {{index}}'
+  }
+});
